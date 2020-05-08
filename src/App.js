c37dd82f3d288d8300f064af2272406c007f508a
tsaristbomba <andrexerdna@gmail.com>
2020-05-08 09:46:39 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Slide from 'react-reveal/Slide'
+import makeCarousel from 'react-reveal/makeCarousel'
+
+import { Container, Arrow, Image, Wrapper, Dot, Dots, Children, Overlay, Title } from "./components/styles"
+
+import img1 from "./imgs/black-4572125_1920.jpg"
+import img2 from "./imgs/blue-2564660_1920.jpg"
+import img3 from "./imgs/girl-1828536_1920.jpg"
+import img4 from "./imgs/model-600225_1920.jpg"
+import img5 from "./imgs/people-2591867_1920.jpg"
+import img6 from "./imgs/person-731142_1920.jpg"
 
 function App() {
+
+  document.getElementById('root').style.height = '100vh'
+  document.getElementById('root').style.backgroundColor = '#333'
+  document.getElementsByTagName('body')[0].style.margin = '0'
+
+  const imgs = [
+    img5,
+    img2,
+    img3,
+    img4,
+    img1,
+    img6
+  ]
+  const CarouselUI = ({ position, handleClick, children, total }) => (
+    <Container>
+      <Children>
+        {children}
+        <Arrow onClick={handleClick} data-position={position - 1}><i class="fas fa-chevron-circle-left"></i></Arrow>
+        <Arrow right onClick={handleClick} data-position={position + 1}><i class="fas fa-chevron-circle-right"></i></Arrow>
+      </Children>
+      
+      <Dots>
+        {Array(...Array(total)).map( (val, index) =>
+          <Dot key={index} onClick={handleClick} data-position={index}>
+            {index === position ? '● ' : '○ ' }
+          </Dot>
+        )}
+      </Dots>
+    </Container>
+  );
+  const Carousel = makeCarousel(CarouselUI);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Slide right><Title>React Slider</Title></Slide>
+      <Carousel defaultWait={0}>
+        {imgs.map((data, i) => {
+            return(
+                <Slide right key={i} >
+                  <Image className="img" style={{ backgroundImage: `url(${data})` }} >
+                      <Overlay className="img-overlay"/>
+                  </Image>
+                </Slide>
+            )
+            }
+          )
+        }
+      </Carousel>
+    </Wrapper>
   );
 }
 
---
+++ src/components/styles.js
@@ -0,0 +1,150 @@
+import styled, { css } from "styled-components"
+  
+const width = '70vw', height='70vh';
+
+export const Container = styled.div`
+    position: relative;
+    overflow: hidden;
+    background-color: #333;
+    display: flex;
+    flex-direction: column;
+    border-radius: 5px;
+    height: ${height};
+    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.3);
+
+
+    @media screen and (max-width: 700px) {
+        width: 100%;
+    }
+    @media screen and (max-height: 700px) {
+        width: 100%;
+    }
+`;
+export const Arrow = styled.div`
+    text-shadow: 1px 1px 1px #333;
+    z-index: 100;
+    line-height: ${height};
+    text-align: center;
+    position: absolute;
+    top: 0;
+    width: 10%;
+    font-size: 3em;
+    cursor: pointer;
+    color: rgb(232,205,136);
+
+    user-select: none;
+    ${props => props.right ? css`left: 90%;` : css`left: 0%;`}
+
+    &:hover {
+        color: #333;
+
+    }
+
+    @media screen and (max-width: 500px) {
+        font-size: 1.8em;
+    }
+    @media screen and (max-height: 500px) {
+        font-size: 1.8em;
+    }
+`;
+
+export const Image = styled.div`
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+    transition: transform .2s;
+
+    &:hover {
+        transform: scale(1.2);
+    }
+
+    @media screen and (max-width: 700px) {
+        width: 100%;
+    }
+    @media screen and (max-height: 700px) {
+        width: 100%;
+    }
+`
+
+export const Overlay = styled.div`
+    width: ${width};
+    height: ${height};
+    background-color: rgba(232,205,136,0.2);
+    transition: background-color .2s;
+
+    &:hover {
+        background-color: transparent;
+    }
+
+    @media screen and (max-width: 700px) {
+        width: 100%;
+    }
+    @media screen and (max-height: 700px) {
+        width: 100%;
+    }
+`
+
+export const Wrapper = styled.div`
+@import url('https://fonts.googleapis.com/css2?family=Jost:wght@300&display=swap');
+    display: flex;
+    align-items: center;
+    height: 100vh;
+    justify-content: center;
+    flex-direction: column;
+    color: rgb(232,205,136);
+
+
+    font-family: 'Jost', sans-serif;
+`
+
+export const Title = styled.h1`
+        font-size: 4em;
+        text-align: center;
+
+        @media screen and (max-height: 500px) {
+            font-size: 2em;
+            margin: 0;
+
+        }
+`
+
+export const Dot = styled.span`
+  font-size: 2em;
+  cursor: pointer;
+  text-shadow: 1px 1px 1px #333;
+  user-select: none;
+  color: rgb(232,205,136);
+
+
+
+  &:hover {
+      color: #333;
+  }
+`;
+export const Dots = styled.span`
+  text-align: center;
+  width: ${width};
+  position: relative;
+  z-index: 100;
+
+    @media screen and (max-width: 700px) {
+        width: 100%;
+    }
+    @media screen and (max-height: 700px) {
+        width: 100%;
+    }
+`;
+
+export const Children  = styled.div`
+  width: ${width};
+  height: ${height};
+  position: relative;
+
+@media screen and (max-width: 700px) {
+      width: 100%;
+}
+@media screen and (max-height: 700px) {
+      width: 100%;
+}
+`;
+
